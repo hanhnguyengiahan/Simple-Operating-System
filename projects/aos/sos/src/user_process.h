d85a5e69945411e9ffdcad1db73c92a30bc35968
Han <z5452011@ad.unsw.edu.au>
2025-10-12 21:24:11 +1100

    feat: create user_process struct

---
+++ projects/aos/sos/src/user_process.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "ut.h"
+#include "mapping.h"
+struct user_process {
+    ut_t *tcb_ut;
+    seL4_CPtr tcb;
+    ut_t *vspace_ut;
+    seL4_CPtr vspace;
+
+    ut_t *ipc_buffer_ut;
+    seL4_CPtr ipc_buffer;
+
+    ut_t *sched_context_ut;
+    seL4_CPtr sched_context;
+
+    cspace_t cspace;
+
+    ut_t *stack_ut;
+    seL4_CPtr stack;
+
+    list_t *paging_objects;
+    list_t *frame_refs;
+    list_t *vm_regions;
+
+    vm_region_t* heap_region;
+    vm_region_t* stack_region;
+
+};
+typedef struct user_process user_process_t;
