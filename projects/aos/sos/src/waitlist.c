decceaf5fac0035097df99b8113e5e9afae6cf0e
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2025-11-27 14:43:59 +1100

    feat: impl sos_process_wait

---
+++ projects/aos/sos/src/waitlist.c
@@ -0,0 +1,79 @@
+#include "waitlist.h"
+
+int init_waitlist(waitlist_t **waitlist_out) {
+    waitlist_t *waitlist = malloc(sizeof(waitlist_t));
+    if (!waitlist) {
+        ZF_LOGE("Failed to alloc waitlist");
+        return -1;
+    }
+    
+    waitlist->ntfns = malloc(sizeof(list_t));
+    if (!waitlist) {
+        ZF_LOGE("Failed to alloc ntfns list");
+        free(waitlist);
+        return -1;
+    }
+    list_init(waitlist->ntfns);
+
+    waitlist->mutex = malloc(sizeof(sync_recursive_mutex_t));
+    if (!waitlist) {
+        ZF_LOGE("Failed to alloc waitlist mutex");
+        free(waitlist->ntfns);
+        free(waitlist);
+        return -1;
+    }
+    sync_recursive_mutex_new(waitlist->mutex);
+
+    *waitlist_out = waitlist;
+    return 0;
+}
+
+int add_waiter(waitlist_t *waitlist, seL4_CPtr src_ntfn) {
+    sync_recursive_mutex_lock(waitlist->mutex);
+
+    // seL4_CPtr copied_ntfn = cspace_alloc_slot(&cspace);
+    // if (copied_ntfn == seL4_CapNull) {
+    //     ZF_LOGE("Failed to alloc slot for ntfn");
+    //     sync_recursive_mutex_unlock(waitlist->mutex);
+    //     return -1;
+    // }
+
+    // seL4_Error err = cspace_copy(&cspace, copied_ntfn, &cspace, src_ntfn, seL4_AllRights);
+    // if (err != seL4_NoError) {
+    //     cspace_free_slot(&cspace, copied_ntfn);
+    //     ZF_LOGE("Failed to copy cap");
+    //     sync_recursive_mutex_unlock(waitlist->mutex);
+    //     return -1;
+    // }
+
+    seL4_CPtr *ntfn_ptr = malloc(sizeof(seL4_CPtr));
+    *ntfn_ptr = src_ntfn;
+
+    list_prepend(waitlist->ntfns, (void*) ntfn_ptr);
+    sync_recursive_mutex_unlock(waitlist->mutex);
+
+    return 0;
+}
+
+int signal_then_destroy_caps(waitlist_t *waitlist) {
+    sync_recursive_mutex_lock(waitlist->mutex);
+    
+    assert(waitlist->ntfns != NULL);
+
+    for (struct list_node *cur = waitlist->ntfns->head; cur != NULL;) {
+        seL4_CPtr *ntfn_ptr = cur->data;
+        seL4_Signal(*ntfn_ptr);
+
+        // let the owner thread of these ntfn free the ntfns themselves
+
+        free(ntfn_ptr);
+        
+        struct list_node *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+
+    sync_recursive_mutex_unlock(waitlist->mutex);
+
+    return 0;
+}
